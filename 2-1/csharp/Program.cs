07a1e33a5c9c2e21e4071c0dbde177ba0c0d1b6d
phil4literacy <philip@learnclick.com>
2019-04-19 10:17:21 +0100

    added c# solution

---
+++ 2-1/csharp/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int row = 1; row <= 4; row++) {
+                for (int hashNum = 1; hashNum <= 10 - row * 2; hashNum++) {
+                    Console.Write("#");
+                }
+                Console.WriteLine("");
+                Console.Write(" ".PadLeft(row));
+            }            
+        }
+    }
+}
